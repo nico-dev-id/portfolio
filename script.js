989f4cec7b0190d4b037d3930eb9c9236c96a63b
nico-dev-id <nico13la@gmail.com>
2026-03-29 10:24:52 +0700

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//console.log("JavaScript berhasil terhubung!");
+
+const heroTitle = document.querySelector(".hero-title");
+const hireButton=document.querySelector(".btn-primary");
+const output=document.querySelector("#output");
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+
+heroTitle.textContent="Hi, I'm Nico - Frontend Developer";
+
+heroTitle.style.color = "black";
+heroTitle.style.fontSize="40px";
+
+/*tombol Hire Me di HERO*/
+hireButton.addEventListener("click",function(){
+    //1. alert
+    alert("Thanks for Visitting My Portfolio!");
+
+    //2. Tambah Element
+    if (output){
+        const newText=document.createElement("p");
+        newText.textContent="Nico Clicked hire Me!";
+
+        newText.addEventListener("click",function(){
+            newText.remove();
+        });
+        output.appendChild(newText);
+    }
+});
+
+/*TOMBOL KONTAK SAAT DI KLIK SROL KE KONTAK*/
+const contactButton=document.querySelector(".btn-secondary");
+contactButton.addEventListener("click", function(){
+    document.querySelector("#contact").scrollIntoView({
+        behavior:"smooth"
+    })
+});
+
+//BURGER MENU
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+//tutup menu setelah klik
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
